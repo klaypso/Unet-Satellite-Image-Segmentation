1efb859ce9983b8cdfac1b01dcc64188b06748bf
klaypso <klaypso@proton.me>
2023-07-24 21:37:47 +0000

    Create .eslintrc

--- continue_train_road_tar.py
+++ continue_train_road_tar.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue 
+Created on Tue Jun 19 10:07:28 2018
+
+@author: alex
+"""
+from up_convolution import convolution,trans_convolve
+import tensorflow as tf
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pickle
+from sklearn.preprocessing import StandardScaler
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+############################################ NETWORK BUILDING #############################
