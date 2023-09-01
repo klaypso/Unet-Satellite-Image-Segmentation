35bdb9316133ba27b7ad4bae2202934a2be538d0
klaypso <klaypso@proton.me>
2023-09-01 21:33:06 +0000

    Regenerate gemspec.

--- continue_train_road_tar.py
+++ continue_train_road_tar.py
@@ -102,4 +102,9 @@ def initialize_parameters():
     left_2_1_conv_bias = tf.get_variable(name = "Road_tar_left_2_1_conv_bias",shape = (64),dtype = tf.float32,trainable = True)
     
     left_2_2_conv = tf.get_variable(name = "Road_tar_left_2_2_conv",shape = (3,3,64,64),dtype = tf.float32,trainable = True)
-    left_2_2_conv_bias = tf.get_variable(name = "Road_tar_left_2_2_conv_bias",shape = (
+    left_2_2_conv_bias = tf.get_variable(name = "Road_tar_left_2_2_conv_bias",shape = (64),dtype = tf.float32,trainable = True)    
+    
+    left_3_1_conv = tf.get_variable(name = "Road_tar_left_3_1_conv",shape = (3,3,64,128),dtype = tf.float32,trainable = True)
+    left_3_1_conv_bias = tf.get_variable(name = "Road_tar_left_3_1_conv_bias",shape = (128),dtype = tf.float32,trainable = True)
+
+    left_3_2_conv = tf.get_variable(name = "Road_tar_left_3_2_conv",shape = (3,
