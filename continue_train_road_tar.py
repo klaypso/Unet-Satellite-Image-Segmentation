9db427e695d951a9245c2283d40096300850ee66
klaypso <klaypso@proton.me>
2023-10-02 18:16:18 +0000

    Update projects.yml

--- continue_train_road_tar.py
+++ continue_train_road_tar.py
@@ -159,4 +159,13 @@ def initialize_parameters():
     weight_parameters["left_1_1_conv"] = left_1_1_conv
     weight_parameters["left_1_1_conv_bias"] = left_1_1_conv_bias
     
-    weight_parameter
+    weight_parameters["left_1_2_conv"] = left_1_2_conv
+    weight_parameters["left_1_2_conv_bias"] = left_1_2_conv_bias
+
+    weight_parameters["left_2_1_conv"] = left_2_1_conv
+    weight_parameters["left_2_1_conv_bias"] = left_2_1_conv_bias    
+    
+    weight_parameters["left_2_2_conv"] = left_2_2_conv
+    weight_parameters["left_2_2_conv_bias"] = left_2_2_conv_bias    
+
+    weight_parameters["left_3_1_conv"] = left
