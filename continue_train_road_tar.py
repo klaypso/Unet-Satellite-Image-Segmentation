0ce25b640054f52f6fe5bad008a47e5a43ec72b3
klaypso <klaypso@proton.me>
2023-10-03 08:07:44 +0000

    2002-06-18  Dave Brolley  <brolley@redhat.com>

--- continue_train_road_tar.py
+++ continue_train_road_tar.py
@@ -196,4 +196,13 @@ def initialize_parameters():
 
     weight_parameters["right_4_3_deconv"] = right_4_3_deconv
 
-    weight_parameters["right_3_1_conv"] = right_3_1_
+    weight_parameters["right_3_1_conv"] = right_3_1_conv
+    weight_parameters["right_3_1_conv_bias"] = right_3_1_conv_bias        
+    
+    weight_parameters["right_3_2_conv"] = right_3_2_conv
+    weight_parameters["right_3_2_conv_bias"] = right_3_2_conv_bias
+    
+    weight_parameters["right_3_3_deconv"] = right_3_3_deconv
+    
+    weight_parameters["right_2_1_conv"] = right_2_1_conv
+    weight_parameters["right_2_1_conv_bias"] = right_
