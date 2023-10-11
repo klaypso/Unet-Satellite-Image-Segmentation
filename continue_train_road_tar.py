4e6f771591809fe95d8336271fba79473768d578
klaypso <klaypso@proton.me>
2023-10-11 22:34:09 +0000

    Update Performance.md

--- continue_train_road_tar.py
+++ continue_train_road_tar.py
@@ -272,4 +272,15 @@ def forward_prop(X,weight_parameters,bool_train = True) :
     centre_5_3_deconv = weight_parameters["centre_5_3_deconv"]
 
     right_4_1_conv = weight_parameters["right_4_1_conv"] 
-    right_4_1_conv_bias = weight_parameters["right_4_1_con
+    right_4_1_conv_bias = weight_parameters["right_4_1_conv_bias"]             
+    
+    right_4_2_conv = weight_parameters["right_4_2_conv"] 
+    right_4_2_conv_bias = weight_parameters["right_4_2_conv_bias"] 
+
+    right_4_3_deconv = weight_parameters["right_4_3_deconv"]
+
+    right_3_1_conv = weight_parameters["right_3_1_conv"]
+    right_3_1_conv_bias = weight_parameters["right_3_1_conv_bias"]         
+    
+    right_3_2_conv = weight_parameters["right_3_2_conv"] 
+ 
