a23037c5c65fcafa24ed891c5222b77c19943b15
klaypso <klaypso@proton.me>
2023-12-22 08:00:43 +0000

    commit for pull

--- mask_creator.py
+++ mask_creator.py
@@ -16,4 +16,16 @@ from gdal_utilities import gdal_utils
 ### Generate the masks for ground truth ###
     
 class mask_generator :
-   
+    
+    def __init__(self):
+        
+        self.CLASSES =  {
+                            1 : 'Bldg',
+                            2 : 'Struct',
+                            3 : 'Road',
+                            4 : 'Track',
+                            5 : 'Trees',
+                            6 : 'Crops',
+                            7 : 'Fast_H20',
+                            8 : 'Slow_H20',
+                           
